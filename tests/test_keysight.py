5a01959e162d36700572860566d33ec2e03455f7
Travis F. Collins <travis.collins@analog.com>
2024-05-15 11:52:34 -0600

    Fix Keysight test
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- tests/test_keysight.py
+++ tests/test_keysight.py
@@ -4,7 +4,7 @@ from bench import keysight
 def test_e36233a_auto():
     ps = keysight.E36233A()
     # Read all properties from all channels
-    for chan in ps.channels:
+    for chan in ps.channels():
         print(f"Channel {chan.channel_number}:")
         print(f"Voltage: {chan.voltage}")
         print(f"Current: {chan.current}")
