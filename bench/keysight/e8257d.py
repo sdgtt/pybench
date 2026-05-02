2817da2770a805454f3f38b13f5ca44780a190d2
Travis F. Collins <travis.collins@analog.com>
2026-05-01 21:08:17 -0400

    Add Keysight signal generators, spectrum analyzers, network analyzers
    New classes covering the most common Keysight bench instruments:
      - N5182B (MXG vector signal generator, <=6 GHz)
      - E8257D (PSG analog signal generator, <=67 GHz)
      - N9020B (MXA signal analyzer)
      - N9030B (PXA signal analyzer)
      - N5232A (PNA-L network analyzer)
      - E5071C (ENA network analyzer)
    
    All inherit from Common and follow the existing pattern (id-matched IDN,
    properties for primary controls).

---
+++ bench/keysight/e8257d.py
@@ -0,0 +1,40 @@
+from bench.common import Common
+
+
+class E8257D(Common):
+    """Keysight E8257D PSG analog signal generator (up to 67 GHz)."""
+
+    id = "E8257D"
+    """Substring returned by IDN query to identify the device"""
+
+    @property
+    def reset(self):
+        """Reset the instrument"""
+        self._instr.write("*RST")
+
+    @property
+    def frequency(self) -> float:
+        """RF carrier frequency in Hz"""
+        return float(self._instr.query(":FREQ?"))
+
+    @frequency.setter
+    def frequency(self, value: float):
+        self._instr.write(f":FREQ {value}")
+
+    @property
+    def power(self) -> float:
+        """Output power level in dBm"""
+        return float(self._instr.query(":POW?"))
+
+    @power.setter
+    def power(self, value: float):
+        self._instr.write(f":POW {value}")
+
+    @property
+    def output_enabled(self) -> bool:
+        """RF output state"""
+        return bool(int(self._instr.query(":OUTP?")))
+
+    @output_enabled.setter
+    def output_enabled(self, value: bool):
+        self._instr.write(f":OUTP {1 if value else 0}")
---
+++ bench/keysight/n9020b.py
@@ -0,0 +1,54 @@
+from bench.common import Common
+
+
+class N9020B(Common):
+    """Keysight N9020B MXA signal analyzer."""
+
+    id = "N9020B"
+    """Substring returned by IDN query to identify the device"""
+
+    _markers = [*range(1, 12 + 1)]
+
+    @property
+    def reset(self):
+        """Reset the instrument"""
+        self._instr.write("*RST")
+
+    @property
+    def center_frequency(self) -> float:
+        """Center frequency in Hz"""
+        return float(self._instr.query(":FREQ:CENT?"))
+
+    @center_frequency.setter
+    def center_frequency(self, value: float):
+        self._instr.write(f":FREQ:CENT {value}")
+
+    @property
+    def span(self) -> float:
+        """Span in Hz"""
+        return float(self._instr.query(":FREQ:SPAN?"))
+
+    @span.setter
+    def span(self, value: float):
+        self._instr.write(f":FREQ:SPAN {value}")
+
+    @property
+    def reference_level(self) -> float:
+        """Reference level in dBm"""
+        return float(self._instr.query(":DISP:WIND:TRAC:Y:RLEV?"))
+
+    @reference_level.setter
+    def reference_level(self, value: float):
+        self._instr.write(f":DISP:WIND:TRAC:Y:RLEV {value}")
+
+    def peak_search_marker(self, marker: int = 1):
+        """Move marker to the maximum peak."""
+        if marker not in self._markers:
+            raise Exception(f"Valid markers are: {self._markers}")
+        self._instr.write(f":CALC:MARK{marker}:MAX")
+
+    def get_marker_amplitude(self, marker: int = 1) -> float:
+        """Read marker Y value (dBm)."""
+        if marker not in self._markers:
+            raise Exception(f"Valid markers are: {self._markers}")
+        return float(self._instr.query(f":CALC:MARK{marker}:Y?"))
